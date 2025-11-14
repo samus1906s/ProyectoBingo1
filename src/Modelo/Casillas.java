f451bfdba51531d9f520bec2e6f5e9aa027c3c49
Reynold <rruizo@est.utn.ac.cr>
2025-11-14 14:51:10 -0600

    Se crea la clase casillas
    Se crea la clase casillas con sus metodos de acceso y constructor

---
+++ src/Modelo/Casillas.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Student
+ */
+public class Casillas {
+    private int valores;
+    private boolean marcados;
+    private boolean disponible;
+
+    public Casillas(int valores, boolean disponible) {
+        this.valores = valores;
+        this.marcados = disponible;
+        this.disponible = disponible;
+    }
+
+    public int getValores() {
+        return valores;
+    }
+
+    public boolean isMarcados() {
+        return marcados;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+    
+    public void marcar() {
+        this.marcados = true;
+    }
+
+    public void desmarcar() {
+        if (!disponible) {
+            this.marcados = false;
+        }
+    }
+}
