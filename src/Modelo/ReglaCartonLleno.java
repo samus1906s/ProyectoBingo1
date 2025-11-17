5849b30b58bd37182f5a8182fa3fd6b0f89a6f27
samus <samuelcedas19@gmail.com>
2025-11-17 00:39:02 -0600

    Se crea la regla de cartón de lleno

---
+++ src/Modelo/ReglaCartonLleno.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author samue
+ */
+public class ReglaCartonLleno implements ReglaVictoria{
+
+    @Override
+    public boolean esGanador(Cartones carton) {
+        Casillas[][] c = carton.getEspacios();
+        for (int i = 0; i < 5; i++){
+            for (int j = 0; j < 5; j++){
+                if(!c[i][j].isMarcados()){
+                    return false;
+                }
+            }
+        }
+      return true;  
+    }   
+}
