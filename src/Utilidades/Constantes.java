307f04d1be6e4cb418b38753efa1476402680312
samus <samuelcedas19@gmail.com>
2025-11-17 01:28:58 -0600

    Se crea la clase de constantes del juego

---
+++ src/Utilidades/Constantes.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Utilidades;
+
+/**
+ *
+ * @author samue
+ */
+public class Constantes {
+    public static final int filasCarton = 5;
+    public static final int columnasCarton = 5;
+    private Constantes() {}
+}
