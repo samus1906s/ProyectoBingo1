3df65edccb6ea36dce0cdef624b7f11706e8bbd3
samus <samuelcedas19@gmail.com>
2025-11-17 00:55:27 -0600

    Se crea la regla de cuatro esquinas

---
+++ src/Modelo/ReglaCuatroEsquinas.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author samue
+ */
+public class ReglaCuatroEsquinas implements ReglaVictoria{
+
+  /*  @Override
+    public boolean esGanador(Cartones carton) {
+         Casillas[][] c = carton.getCasillas;
+        return c[0][0].isMarcado()
+            && c[0][4].isMarcado()
+            && c[4][0].isMarcado()
+            && c[4][4].isMarcado();
+    }
+    }
+    
+    
+}
