0faa19d0c16026b57995a4ab9429eea04d01d96d
Brandon Valdelomar <brvaldelomarco@est.utn.ac.cr>
2025-11-17 12:01:47 -0600

    WrapLayout
    Se implementa clase wraplayout para acomodar cartones y diaglogs

---
+++ src/Modelo/WrapLayout.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Insets;
+
+/**
+ *
+ * @author Valdelomaar
+ */
+public class WrapLayout extends FlowLayout {
+    public WrapLayout() {
+        super();
+    }
+
+    public WrapLayout(int align) {
+        super(align);
+    }
+
+    public WrapLayout(int align, int hgap, int vgap) {
+        super(align, hgap, vgap);
+    }
+
+    @Override
+    public Dimension preferredLayoutSize(Container target) {
+        return layoutSize(target, true);
+    }
+
+    @Override
+    public Dimension minimumLayoutSize(Container target) {
+        Dimension minimum = layoutSize(target, false);
+        minimum.width -= (getHgap() + 1);
+        return minimum;
+    }
+
+    private Dimension layoutSize(Container target, boolean preferred) {
+        synchronized (target.getTreeLock()) {
+            int targetWidth = target.getSize().width;
+            if (targetWidth == 0)
+                targetWidth = Integer.MAX_VALUE;
+
+            int hgap = getHgap();
+            int vgap = getVgap();
+            Insets insets = target.getInsets();
+            int maxWidth = targetWidth - (insets.left + insets.right + hgap * 2);
+
+            Dimension dim = new Dimension(0, 0);
+            int rowWidth = 0;
+            int rowHeight = 0;
+
+            int nmembers = target.getComponentCount();
+            for (int i = 0; i < nmembers; i++) {
+                Component m = target.getComponent(i);
+                if (m.isVisible()) {
+                    Dimension d = preferred ? m.getPreferredSize() : m.getMinimumSize();
+
+                    if (rowWidth + d.width > maxWidth) {
+                        dim.width = Math.max(dim.width, rowWidth);
+                        dim.height += rowHeight + vgap;
+                        rowWidth = 0;
+                        rowHeight = 0;
+                    }
+
+                    rowWidth += d.width + hgap;
+                    rowHeight = Math.max(rowHeight, d.height);
+                }
+            }
+
+            dim.width = Math.max(dim.width, rowWidth);
+            dim.height += rowHeight;
+
+            dim.width += insets.left + insets.right;
+            dim.height += insets.top + insets.bottom;
+
+            return dim;
+        }
+    }
+}
